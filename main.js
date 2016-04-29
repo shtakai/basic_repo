039f48809dd801277b2046ee61fc261e4dc4b309
Alyson Takai <shtakai@gmail.com>
2016-04-29 11:15:27 +0900

    added fish

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
   $('button').click(function() {
-    $('p').append("clicked");
+    $('p').append("*");
+    $('img').toggle();
     console.log('click');
   });
 })
